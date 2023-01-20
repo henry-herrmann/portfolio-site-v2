9e9b35fd825602d874460d4cf2a617c14250e345
henry-herrmann <henryherrmann.tv@gmail.com>
2023-01-20 18:49:33 +0100

    404 page

--- pages/404.js
+++ pages/404.js
@@ -4,6 +4,7 @@ import { Fragment} from 'react'
 import LandingHeader from '../components/LandingHeader';
 import BlockContent from '../components/block/BlockContent';
 import Footer from '../components/Footer';
+import Link from 'next/link';
 
 export default function FourHundredFour(){
     return (
@@ -14,13 +15,14 @@ export default function FourHundredFour(){
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="icon" href="/logo/logo.jpg" />
             </Head>
-            <div className='min-h-screen bg-gradient-to-r from-gradient-1 to-gradient-2'>
+            <div className='min-h-screen'>
                 <LandingHeader />
-                <div className="flex justify-center flex-col ">
+                <div className="flex justify-center flex-col bg-herrmann-gray-light">
                     <div className='flex justify-center'>
                         <div className='mr-8 ml-8 text-center max-w-screen-md mt-44 sm:mt-40 ml:m-0 mr:m-0 mb-[43vh] md:mb-[54.8vh]'>
                             <div className='font-bold text-5xl mb-4 mr-4 ml-4'>Page not found! :(</div>
                             <p>Please check your URL and go back to the home page.</p>
+                            <Link href="app://campus_app">Campus App</Link>
                         </div>
                     </div>
                 </div>
